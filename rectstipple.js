c4ed315d934ff1fb1679f695c1e4e932d4045281
Liam Murphy <liampm32@gmail.com>
2022-04-02 21:50:41 +1100

    Add `Rects` and `Rect Stipple` modes

---
+++ rectstipple.js
@@ -0,0 +1,172 @@
+importScripts("helpers.js");
+
+postMessage([
+    "sliders",
+    defaultControls.concat([{
+        label: "Dots Per Black Pixel",
+        value: 0.000001,
+        min: 0,
+        max: 0.0002,
+        step: 0.000001,
+    }]),
+]);
+
+function getCircles(getPixel, rect, numDots, dotsPerPx) {
+    function getDarkness(x, y) {
+        let darkness = getPixel(x, y);
+        if (x == Math.floor(rect.left)) {
+            darkness *= x + 1 - rect.left;
+        } else if (x == Math.ceil(rect.right) - 1) {
+            darkness *= rect.right - x;
+        }
+
+        if (y == Math.floor(rect.top)) {
+            darkness *= y + 1 - rect.top;
+        } else if (y == Math.ceil(rect.bottom) - 1) {
+            darkness *= rect.bottom - y;
+        }
+
+        return darkness;
+    }
+
+    if (numDots == 1) {
+        // Find the average position in this rectangle weighted by each pixel's darkness.
+        let totalDarkness = 0;
+        let weightedSumX = 0;
+        let weightedSumY = 0;
+
+        for (let x = Math.floor(rect.left); x < rect.right; x++) {
+            for (let y = Math.floor(rect.top); y < rect.bottom; y++) {
+                const darkness = getDarkness(x, y);
+                totalDarkness += darkness;
+                weightedSumX += darkness * x;
+                weightedSumY += darkness * y;
+            }
+        }
+
+        let weightedAvgX = weightedSumX / totalDarkness;
+        let weightedAvgY = weightedSumY / totalDarkness;
+        return [
+            [
+                weightedAvgX,
+                weightedAvgY,
+                1, // radius
+            ],
+        ];
+    } else {
+        const width = rect.right - rect.left;
+        const height = rect.bottom - rect.top;
+
+        // The number of dots we want to be in the first subdivision.
+        const targetDots = Math.floor(numDots / 2);
+
+        let dotsSoFar = 0;
+        if (width > height) {
+            for (let x = Math.floor(rect.left); x < rect.right; x++) {
+                let dotsThisColumn = 0;
+
+                for (let y = Math.floor(rect.top); y < rect.bottom; y++) {
+                    dotsThisColumn += getDarkness(x, y) * dotsPerPx;
+                }
+
+                dotsSoFar += dotsThisColumn;
+                if (dotsSoFar >= targetDots) {
+                    // The dividing line falls somewhere within this row.
+                    // Place it based on how much we overshot.
+                    const overshoot = dotsSoFar - targetDots;
+                    const overshootProportion = overshoot / dotsThisColumn;
+                    const divisionX = x + (1 - overshootProportion);
+
+                    let lines = [];
+
+                    lines = lines.concat(getCircles(
+                        getPixel,
+                        { ...rect, right: divisionX },
+                        targetDots,
+                        dotsPerPx,
+                    ));
+
+                    lines = lines.concat(getCircles(
+                        getPixel,
+                        { ...rect, left: divisionX },
+                        numDots - targetDots,
+                        dotsPerPx,
+                    ));
+
+                    return lines;
+                }
+            }
+        } else {
+            for (let y = Math.floor(rect.top); y < rect.bottom; y++) {
+                let dotsThisRow = 0;
+
+                for (let x = Math.floor(rect.left); x < rect.right; x++) {
+                    dotsThisRow += getDarkness(x, y) * dotsPerPx;
+                }
+
+                dotsSoFar += dotsThisRow;
+                if (dotsSoFar >= targetDots) {
+                    // The dividing line falls somewhere within this row.
+                    // Place it based on how much we overshot.
+                    const overshoot = dotsSoFar - targetDots;
+                    const overshootProportion = overshoot / dotsThisRow;
+                    const divisionY = y + (1 - overshootProportion);
+
+                    let lines = [];
+
+                    lines = lines.concat(getCircles(
+                        getPixel,
+                        { ...rect, bottom: divisionY },
+                        targetDots,
+                        dotsPerPx,
+                    ));
+
+                    lines = lines.concat(getCircles(
+                        getPixel,
+                        { ...rect, top: divisionY },
+                        numDots - targetDots,
+                        dotsPerPx,
+                    ));
+
+                    return lines;
+                }
+            }
+        }
+    }
+}
+
+onmessage = function (e) {
+    const [config, pixData] = e.data;
+
+    // Slider variables
+    const dotsPerPx = parseFloat(config["Dots Per Black Pixel"]);
+
+    // Image processor (gets a value for the current pixel)
+    const getPixel = pixelProcessor(config, pixData);
+
+    let totalDarkness = 0;
+    for (let x = 0; x < config.width; x++) {
+        for (let y = 0; y < config.height; y++) {
+            totalDarkness += getPixel(x, y);
+        }
+    }
+
+    // Figure out the integer number of dots we want and adjust the dots per pixel slightly so we get that number,
+    const numDots = Math.round(dotsPerPx * totalDarkness);
+    const adjustedDotsPerPx = numDots / totalDarkness;
+
+    const circles = getCircles(
+        getPixel,
+        {
+            left: 0,
+            top: 0,
+            right: config.width,
+            bottom: config.height,
+        },
+        numDots,
+        adjustedDotsPerPx,
+    );
+    postCircles(
+        circles,
+    );
+};
